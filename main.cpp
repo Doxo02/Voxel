038bb7a5e27cc2e149610c0b7a668bd751060b1e
Lars Zell <lars.zell@gmail.com>
2025-03-22 18:01:49 +0100

    Basic OpenGL setup

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+int main(int, char**){
+    if(!glfwInit()){
+        return -1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if(!window){
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if(glewInit() != GLEW_OK){
+        return -1;
+    }
+
+    while(!glfwWindowShouldClose(window)){
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f(0.0f, 0.5f);
+        glVertex2f(0.5f, -0.5f);
+        glEnd();
+
+        glfwSwapBuffers(window);
+
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
