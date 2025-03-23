4176ecbc890ad7fa22b1e93724d08b93a746440d
Lars Zell <lars.zell@gmail.com>
2025-03-23 20:35:42 +0100

    Basic BrickMap

---
+++ DataStructures/BrickMap.h
@@ -0,0 +1,50 @@
+#ifndef BRICKMAP_H
+#define BRICKMAP_H
+
+#include <vector>
+#include <unordered_map>
+#include <glm/glm.hpp>
+#include <array>
+
+struct BrickMap {
+    uint64_t solidMask[8] = {
+        0x0000000000000000,
+        0x0000000000000001,
+        0x0000000000000003,
+        0x0000000000000007,
+        0x000000000000000F,
+        0x000000000000001F,
+        0x000000000000003F,
+        0x000000000000007F
+    };
+
+    uint32_t colorPointer;
+    uint8_t LODColor[3];
+};
+
+class BrickGrid {
+    std::vector<BrickMap> bricks;
+
+public:
+    BrickGrid(int size) {
+        bricks.resize(size * size * size);
+    }
+
+    BrickMap& getBrick(int x, int y, int z) {
+        return bricks[x + y * 16 + z * 256];
+    }
+
+    void setBrick(int x, int y, int z, const BrickMap& brick) {
+        bricks[x + y * 16 + z * 256] = brick;
+    }
+
+    int getSize() {
+        return bricks.size();
+    }
+
+    std::vector<BrickMap>& getBricks() {
+        return bricks;
+    }
+};
+
+#endif // BRICKMAP_H
